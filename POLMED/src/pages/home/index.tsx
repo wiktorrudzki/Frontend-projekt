7e45486525e4a063538652e373eecade421ef6e2
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-11-29 23:41:37 +0100

    added rest of homepage for desktop

--- POLMED/src/pages/home/index.tsx
+++ POLMED/src/pages/home/index.tsx
@@ -1,5 +1,7 @@
 import HomePageFooter from "@/components/footers/home-page";
 import { useRef } from "react";
+import building from "@/img/building.jpg";
+import NFZLogo from "@/img/NFZ_logo.svg";
 import "./styles/style.scss";
 import "./styles/home-up.scss";
 import "./styles/home-down.scss";
@@ -28,6 +30,17 @@ const Home = () => {
       </section>
       <div ref={hpDownRef} className="hp-down">
         <div onClick={scrollUp} className="hp-down-triangle"></div>
+        <section className="hp-down-section">
+          <div className="hp-down-section-info">
+            <p className="hp-down-section-text">
+              Dobro naszych pacjentów jest dla nas najważniejsze, dlatego placówka POLmed
+              współpracuje z Narodowym Funduszem Zdrowia w celu zmniejszenia kosztów prowadzonych
+              usług.{" "}
+            </p>
+            <img className="hp-down-section-logo" src={NFZLogo} alt="nfz logo" />
+          </div>
+          <img className="hp-down-section-building" src={building} alt="building" />
+        </section>
         <HomePageFooter />
       </div>
     </div>
