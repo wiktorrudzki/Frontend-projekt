d561678581e7d463070110697cdd48c3b1bb77bd
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-11-29 19:26:53 +0100

    added footer to homepage

--- POLMED/src/components/footers/home-page/index.tsx
+++ POLMED/src/components/footers/home-page/index.tsx
@@ -1,35 +1,35 @@
 import logoWhite from "@/img/logo/logo_white.png";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./style.scss";
 
 const HomePageFooter = () => {
   return (
-    <footer className="home-page-footer">
-      <section className="home-page-footer-upper">
-        <ul className="home-page-footer-ul">
-          <li className="home-page-footer-li">
-            <img className="home-page-footer-logo" src={logoWhite} alt="logo" />
+    <footer className="hp-footer">
+      <section className="hp-footer-upper">
+        <ul className="hp-footer-ul">
+          <li className="hp-footer-li">
+            <img className="hp-footer-logo" src={logoWhite} alt="logo" />
           </li>
-          <li className="home-page-footer-li">
+          <li className="hp-footer-li">
             <p>ul. Pawia 5</p>
           </li>
-          <li className="home-page-footer-li">
+          <li className="hp-footer-li">
             <p>31-153 Kraków</p>
           </li>
         </ul>
-        <ul className="home-page-footer-ul">
-          <li className="home-page-footer-li">
-            <Link className="home-page-footer-link" to="/ourDoctors">
+        <ul className="hp-footer-ul">
+          <li className="hp-footer-li">
+            <Link className="hp-footer-link" to="/ourDoctors">
               Nasi lakerze
             </Link>
           </li>
-          <li className="home-page-footer-li">
-            <Link className="home-page-footer-link" to="/pricing">
+          <li className="hp-footer-li">
+            <Link className="hp-footer-link" to="/pricing">
               Cennik
             </Link>
           </li>
-          <li className="home-page-footer-li">
-            <Link className="home-page-footer-link" to="/contact">
+          <li className="hp-footer-li">
+            <Link className="hp-footer-link" to="/contact">
               Kontakt
             </Link>
           </li>
