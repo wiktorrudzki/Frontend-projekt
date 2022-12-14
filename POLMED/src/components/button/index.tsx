be9f73a2f7603125e36c084f2ddf627cd0267efa
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-12-14 13:20:45 +0100

    added optional onClick props in button

--- POLMED/src/components/button/index.tsx
+++ POLMED/src/components/button/index.tsx
@@ -1,10 +1,15 @@
 type Props = {
-    content: string;
-    btnType: "btn-primary" | "btn-secondary" | "btn-tertiary" | "btn-warning";
+  content: string;
+  btnType: "btn-primary" | "btn-secondary" | "btn-tertiary" | "btn-warning";
+  onClick?: () => void;
 };
 
-const Button = ({content, btnType}:Props) => {
-    return <button className={`btn ${btnType}`}>{content}</button>;
+const Button = ({ content, btnType, onClick }: Props) => {
+  return (
+    <button onClick={onClick} className={`btn ${btnType}`}>
+      {content}
+    </button>
+  );
 };
 
 export default Button;
