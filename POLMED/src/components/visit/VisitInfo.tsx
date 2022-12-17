f0f650b7d31682789da3f88867588a80c99fd50b
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-12-17 22:47:25 +0100

    fixed displaying data in VisitInfo component

--- POLMED/src/components/visit/VisitInfo.tsx
+++ POLMED/src/components/visit/VisitInfo.tsx
@@ -7,7 +7,7 @@ const VisitInfo = ({ iconClassName, content }: Props) => {
   return (
     <div className="visit-feature">
       <i className={`visit-feature--icon ${iconClassName}`}></i>
-      <p>{content}</p>
+      <p>{content.slice(0, -3)}</p>
     </div>
   );
 };
