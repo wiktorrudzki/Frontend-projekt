716e554011a5d1c02de5077d4356def3ddf247b0
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-12-19 00:48:55 +0100

    fixed bug with myAccount and ourDoctors

--- POLMED/src/pages/ourDoctors/slides/VisitSummary.tsx
+++ POLMED/src/pages/ourDoctors/slides/VisitSummary.tsx
@@ -18,43 +18,45 @@ const VisitSummary = ({ sliderRef }: Props) => {
     navigate("/");
   };
 
-  console.log(allVisits);
-
   return (
     <div className="visit-summary-page slide">
-      <SlideWrapper
-        sliderRef={sliderRef}
-        titleLeft="Podsumowanie:"
-        titleRight="Dziękujemy za rejestrację online!"
-        clearWhenBack={false}
-        currentDoctor={
-          <Doctor
-            photo={allVisits[allVisits.length - 1].doctor.photo}
-            name={allVisits[allVisits.length - 1].doctor.name}
-            type={allVisits[allVisits.length - 1].doctor.type}
-            price={allVisits[allVisits.length - 1].price}
-            isBooked={true}
-            reason={allVisits[allVisits.length - 1].reason}
-            date={new Date(allVisits[allVisits.length - 1].date).toLocaleString()}
+      {allVisits.length > 0 && (
+        <>
+          <SlideWrapper
+            sliderRef={sliderRef}
+            titleLeft="Podsumowanie:"
+            titleRight="Dziękujemy za rejestrację online!"
+            clearWhenBack={false}
+            currentDoctor={
+              <Doctor
+                photo={allVisits[allVisits.length - 1].doctor.photo}
+                name={allVisits[allVisits.length - 1].doctor.name}
+                type={allVisits[allVisits.length - 1].doctor.type}
+                price={allVisits[allVisits.length - 1].price}
+                isBooked={true}
+                reason={allVisits[allVisits.length - 1].reason}
+                date={new Date(allVisits[allVisits.length - 1].date).toLocaleString()}
+              />
+            }
+            rightContent={
+              <div className="visit-summary-right-content">
+                {isLoggedIn ? (
+                  <p>
+                    W celu <Link to="/myAccount">anulowania</Link> wizyty przejdź do zakładki “Moje
+                    konto” lub skontaktuj się z nami telefonicznie
+                  </p>
+                ) : (
+                  <p>
+                    W celu <span>anulowania</span> wizyty skontaktuj się z nami telefonicznie
+                  </p>
+                )}
+                <i></i>
+              </div>
+            }
           />
-        }
-        rightContent={
-          <div className="visit-summary-right-content">
-            {isLoggedIn ? (
-              <p>
-                W celu <Link to="/myAccount">anulowania</Link> wizyty przejdź do zakładki “Moje
-                konto” lub skontaktuj się z nami telefonicznie
-              </p>
-            ) : (
-              <p>
-                W celu <span>anulowania</span> wizyty skontaktuj się z nami telefonicznie
-              </p>
-            )}
-            <i></i>
-          </div>
-        }
-      />
-      <Button onClick={handleEndReservation} content="Strona Główna" btnType="btn-primary" />
+          <Button onClick={handleEndReservation} content="Strona Główna" btnType="btn-primary" />
+        </>
+      )}
     </div>
   );
 };
